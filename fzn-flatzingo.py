ef7b1da8994991e880c0e086ed39fdcde1d043c5
MaxOstrowski <ostrowsk@cs.uni-potsdam.de>
2020-06-24 10:52:40 +0200

    rudimentary output and additional options

--- fzn-flatzingo.py
+++ fzn-flatzingo.py
@@ -42,11 +42,43 @@ def main():
     if args.n is not None:
         num_models = args.n
     clingcon_command.append(str(num_models))
+    if args.s:
+        clingcon_command.append("--stats=2")
+    if args.p is not None:
+        clingcon_command.append("-t{}".format(args.p))
+    if args.r is not None:
+        clingcon_command.append("--seed={}".format(args.r))
+    if args.t is not None:
+        clingcon_command.append("--time={}".format(args.t/1000))
 
     with Popen(["fzn2lp", args.flatzinc], stdout=PIPE) as fzn2lp:
         with Popen(clingcon_command, stdin=fzn2lp.stdout, bufsize=1, universal_newlines=True, stdout=PIPE) as clingcon:
+            answer = False
+            assignment = False
+            complete = False
             for line in clingcon.stdout:
-                print(line, end='')
+                if answer:
+                    # also check for bool output vars
+                    answer = False
+                elif assignment:
+                    # TODO: for array output annotations we need the annotations and we need to parse the solution and reconvert it
+                    assignment = False
+                    print("----------")
+                    sys.stdout.flush()
+                elif "UNSATISFIABLE" in line:
+                    print("=====UNSATISFIABLE=====")
+                    sys.stdout.flush()
+                elif "Models" in line:
+                    if not line.endswith("+"):
+                        complete = True
+                elif "Answer:" in line:
+                    answer = True
+                elif "Assignment:" in line:
+                    assignment = True
+                print(line, end='') # debug
+                sys.stdout.flush()
+            if complete:
+                print("==========")
                 sys.stdout.flush()
 
 if __name__ == "__main__":
