0c87e086c0b9bdf9bf921a3d1c8f18fdd981df3e
MaxOstrowski <ostrowsk@cs.uni-potsdam.de>
2020-06-30 16:14:40 +0200

    make encoding relative

--- fzn-flatzingo.py
+++ fzn-flatzingo.py
@@ -90,7 +90,7 @@ def main():
         raise Exception("Option -n is not allowed on optimization problems")
 
     #clingcon_command = [os.path.join(sys.path[0],"clingcon"), "encoding.lp", "-"]
-    clingcon_command = ["clingcon", "encoding.lp", "-"]
+    clingcon_command = ["clingcon", os.path.join(sys.path[0],"encoding.lp"), "-"]
     num_models = 1
     if args.a:
         num_models = 0
