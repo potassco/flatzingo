1849a0b38aadbcc66ab544de235bbc317a6c69e5
MaxOstrowski <ostrowsk@cs.uni-potsdam.de>
2021-05-06 13:36:33 +0200

    test and corrected bool_and

--- tests/test_mzn.py
+++ tests/test_mzn.py
@@ -72,3 +72,4 @@ def test_mzn_fast():
     check("tests/mzn/array_var_bool_element_1.mzn", [[f"b={b}"] + toBool(a1,"a1",a2,"a2",a3,"a3",a4,"a4",a5,"a5",a6,"a6",a7,"a7",a8,"a8",a9,"a9",c,"c") for b,a1,a2,a3,a4,a5,a6,a7,a8,a9,c in itertools.product(range(1,10), BOOL, BOOL, BOOL, BOOL, BOOL, BOOL, BOOL, BOOL, BOOL, BOOL) if eval(f'a{b}')==c ])
     # this only works for -O0 currently, minizinc bug
     check("tests/mzn/bool2int_1.mzn", [[f"b={b}"] + toBool(c,"c") for b,c in itertools.product(range(0,10), BOOL) if (c and b == 1) or (not c and b == 0)])
+    check("tests/mzn/bool_and_1.mzn", [toBool(a,"a",b,"b",r,"r") for a,b,r in itertools.product(BOOL, BOOL, BOOL) if (r and (a and b) or (not r and not (a and b)))])
